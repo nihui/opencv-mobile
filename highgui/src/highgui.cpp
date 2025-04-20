db7b63d4cf3f97bee4fed468911a82df37f3303c
nihui <shuizhuyuanluo@126.com>
2025-04-20 22:02:13 +0800

    hw jpg encoder for orion o6

--- highgui/src/highgui.cpp
+++ highgui/src/highgui.cpp
@@ -54,6 +54,9 @@
 #if CV_WITH_RPI
 #include "jpeg_encoder_v4l_rpi.h"
 #endif
+#if CV_WITH_CIX
+#include "jpeg_encoder_v4l_cix.h"
+#endif
 #if defined __linux__ && !__ANDROID__
 #include "display_fb.h"
 #endif
@@ -454,6 +457,56 @@ bool imwrite(const String& filename, InputArray _img, const std::vector<int>& pa
                     }
                 }
             }
+            // fallback to stb_image_write
+        }
+#endif
+#if CV_WITH_CIX
+        if (jpeg_encoder_v4l_cix::supported(img.cols, img.rows, c))
+        {
+            // anything to bgr
+            if (!img.isContinuous())
+            {
+                img = img.clone();
+            }
+
+            int quality = 95;
+            for (size_t i = 0; i < params.size(); i += 2)
+            {
+                if (params[i] == IMWRITE_JPEG_QUALITY)
+                {
+                    quality = params[i + 1];
+                    break;
+                }
+            }
+
+            // cache jpeg_encoder_v4l_cix context
+            static int old_w = 0;
+            static int old_h = 0;
+            static int old_ch = 0;
+            static int old_quality = 0;
+            static jpeg_encoder_v4l_cix e;
+            if (img.cols == old_w && img.rows == old_h && c == old_ch && quality == old_quality)
+            {
+                int ret = e.encode(img.data, filename.c_str());
+                if (ret == 0)
+                    return true;
+            }
+            else
+            {
+                int ret = e.init(img.cols, img.rows, c, quality);
+                if (ret == 0)
+                {
+                    ret = e.encode(img.data, filename.c_str());
+                    if (ret == 0)
+                    {
+                        old_w = img.cols;
+                        old_h = img.rows;
+                        old_ch = c;
+                        old_quality = quality;
+                        return true;
+                    }
+                }
+            }
 
             // fallback to stb_image_write
         }
@@ -798,6 +851,39 @@ bool imencode(const String& ext, InputArray _img, std::vector<uchar>& buf, const
             }
 
             jpeg_encoder_v4l_rpi e;
+            int ret = e.init(img.cols, img.rows, c, quality);
+            if (ret == 0)
+            {
+                ret = e.encode(img.data, buf);
+                if (ret == 0)
+                {
+                    e.deinit();
+                    return true;
+                }
+            }
+            // fallback to stb_image_write
+        }
+#endif
+#if CV_WITH_CIX
+        if (jpeg_encoder_v4l_cix::supported(img.cols, img.rows, c))
+        {
+            // anything to bgr
+            if (!img.isContinuous())
+            {
+                img = img.clone();
+            }
+
+            int quality = 95;
+            for (size_t i = 0; i < params.size(); i += 2)
+            {
+                if (params[i] == IMWRITE_JPEG_QUALITY)
+                {
+                    quality = params[i + 1];
+                    break;
+                }
+            }
+
+            jpeg_encoder_v4l_cix e;
             int ret = e.init(img.cols, img.rows, c, quality);
             if (ret == 0)
             {
